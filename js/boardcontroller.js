9cc1c89545b0fb9eed876569d6e364b3e3c94f56
AnthonyCorrado <amcstudios@yahoo.com>
2014-08-04 01:04:29 -0700

    markers working

--- js/boardcontroller.js
+++ js/boardcontroller.js
@@ -1,3 +1,29 @@
+var mark = "X";
+var turnNum = 0;
+
 app.controller('boardController', function ($scope) {
-	$scope.boxrows = [['','','','','','','',''],['','','','','','','',''],['','','','','','','','']];
+	$scope.boxrows = [[null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null]];
+	console.log($scope.boxrows);
+
+	altTurn = function () {
+		if (turnNum % 2 === 0) {
+			mark = "O";
+			// p2TimerReset();
+		}
+		else {
+			mark = "X";
+			// p1TimerReset();
+		}
+		turnNum += 1;
+		console.log(mark);
+		};
+
+	$scope.makeMove = function(r, c) {
+		cell = $scope.boxrows[r][c];
+		if (!cell) {
+      $scope.boxrows[r][c] = mark;
+      altTurn();
+    }
+	};
+
 });
