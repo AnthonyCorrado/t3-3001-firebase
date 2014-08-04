17a322690e99b52540bc62dc4eabb72549297a6b
AnthonyCorrado <amcstudios@yahoo.com>
2014-08-03 22:01:10 -0700

    added timer

--- js/boardcontroller.js
+++ js/boardcontroller.js
@@ -1,3 +1,3 @@
 app.controller('boardController', function ($scope) {
-	$scope.boxrows = [['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x']];
+	$scope.boxrows = [['','','','','','','',''],['','','','','','','',''],['','','','','','','','']];
 });
